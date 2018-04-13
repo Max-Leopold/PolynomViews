e981508f8bd2c31a3886708de91f0870f36b10ca
Max <max.leopold2111@gmail.com>
2018-04-13 12:36:40 +0200

    Polynom Dokumantation hinzugefügt

--- src/sample/Polynom.java
+++ src/sample/Polynom.java
@@ -1,7 +1,5 @@
 package sample;
 
-import javafx.scene.paint.Color;
-
 import java.util.Observable;
 
 public class Polynom extends Observable {
@@ -11,6 +9,7 @@ public class Polynom extends Observable {
     double c;
     double d;
 
+    //initialisierung des Polynoms mit übergebenen Werten
     public Polynom(double a, double b, double c, double d){
 
         this.a = a;
@@ -19,26 +18,47 @@ public class Polynom extends Observable {
         this.d = d;
     }
 
+    /**
+     *Ändert a zu dem übergebenen Wert und ruft die setUpdate Methode auf
+     * @param a Wert zu dem a geändert werden soll
+     */
     public void setA(double a) {
         this.a = a;
         setUpdate();
     }
 
+    /**
+     *Ändert b zu dem übergebenen Wert und ruft die setUpdate Methode auf
+     * @param b Wert zu dem b geändert werden soll
+     */
     public void setB(double b) {
         this.b = b;
         setUpdate();
     }
 
+    /**
+     *Ändert c zu dem übergebenen Wert und ruft die setUpdate Methode auf
+     * @param c Wert zu dem c geändert werden soll
+     */
     public void setC(double c) {
         this.c = c;
         setUpdate();
     }
 
+    /**
+     *Ändert d zu dem übergebenen Wert und ruft die setUpdate Methode auf
+     * @param d Wert zu dem d geändert werden soll
+     */
     public void setD(double d) {
         this.d = d;
         setUpdate();
     }
 
+    /**
+     * Berechnet Y Wert des Polynoms an der Stelle i
+     * @param i
+     * @return Y Wert des Polynoms an der Stelle i
+     */
     public double getValueY(int i){
         return (a + b * i + c * Math.pow(i, 2) + d * Math.pow(i, 3));
     }
